850ed4468cf983c837f8b96a9878e67bb7b3a03e
Aditya Kakad <202052302@iiitvadodara.ac.in>
2022-05-26 13:54:26 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './button/button.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './add-task/add-task.component';
+import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
+const appRoutes: Routes = [
+  {path:'',component:TasksComponent},
+  {path:'about',component:AboutComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
